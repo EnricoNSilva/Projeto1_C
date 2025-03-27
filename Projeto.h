dc22b00ab5f44a11aa7f16843b9d9e3ab62dd7a9
Enrico <enriconascimento9@gmail.com>
2025-03-26 21:49:19 -0300

    Numero maximo de tarefas estava errado

---
+++ Projeto.h
@@ -0,0 +1,20 @@
+#define TOTAL_TAREFAS 100 //  numero maximo de tarefas estava em 5, porem em ListaDeTarefa tem tamanho 100
+
+typedef struct {
+    int prioridade;
+    char descricao[300];
+    char categoria[100];
+} Tarefa;
+
+typedef struct {
+    Tarefa tarefas[100];
+    int qtd;
+} ListaDeTarefa;
+
+int criarTarefa(ListaDeTarefas *lt);
+int deletarTarefa(ListaDeTarefas *lt);
+int listarTarefas(ListaDeTarefas *lt);
+
+int carregarTarefas(ListaDeTarefas *lt, char *nomes);
+int salvarTarefas(ListaDeTarefas *lt, char *nome);
+void exibeMenu();
