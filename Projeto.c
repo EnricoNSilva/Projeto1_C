a9d42f4792204039216ff4d0c8fdf5711e60b40d
Enrico Nascimento SIlva <enriconascimento9@gmail.com>
2025-04-02 11:16:58 -0300

    A função strcpy estava copiando o valor da descrição para categoria

--- Projeto.c
+++ Projeto.c
@@ -35,7 +35,7 @@ int deletarTarefa(ListaDeTarefas *lt){
    
    for (; pos<lt->qtd-1; pos++){
    	lt->tarefas[pos].prioridade=lt->tarefas[pos+1].prioridade; 
-	   strcpy(lt->tarefas[pos].descricao, lt->tarefas[pos+1].categoria); 
+	   strcpy(lt->tarefas[pos].descricao, lt->tarefas[pos+1].descricao);  // estava copiando descrição para categoria
 	   strcpy(lt->tarefas[pos].categoria, lt->tarefas[pos+1].categoria);
 
    }
