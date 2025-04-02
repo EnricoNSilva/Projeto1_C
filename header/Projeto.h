1498c01cc071a6eb61f03329b3ae98b5f147ee31
Guilherme S Xavier <guisx322@gmail.com>
2025-04-02 15:01:24 -0300

    FUNCAO DE FILTRAR ATIVIDADES
    Aplicação de uma nova funcionalidades para filtrar tarefas existentes listadas

---
+++ header/Projeto.h
@@ -0,0 +1,27 @@
+#define TOTAL_TAREFAS 100 
+// numero maximo de tarefas estava em 5, porem em ListaDeTarefa tem tamanho 100
+
+typedef struct {
+    int prioridade;
+    char descricao[150];
+    char categoria[50];
+    int dia;
+    int mes;
+    int ano;
+} Tarefa;
+
+typedef struct {
+    Tarefa tarefas[100];
+    int qtd;
+} 
+
+ListaDeTarefas; // o chamado da ListaDeTarefas estava com erro de digitação
+
+int criarTarefa(ListaDeTarefas *lt);
+int deletarTarefa(ListaDeTarefas *lt);
+int listarTarefas(ListaDeTarefas *lt);
+
+int carregarTarefas(ListaDeTarefas *lt, char *nome); // Carregar tarefas estava recebendo //um ponteiro chamado *nomes, o correto seria *nome
+int salvarTarefas(ListaDeTarefas *lt, char *nome);
+void exibeMenu();
+int alterarTarefa(ListaDeTarefas *lt);
